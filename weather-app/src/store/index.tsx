35c43fa89835d619be91eb8dfcc31c1e25b30aa4
Tommy Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-07-02 14:22:23 +0100

    weather

--- weather-app/src/store/index.tsx
+++ weather-app/src/store/index.tsx
@@ -94,8 +94,9 @@ export function App (props: IAppProps) {
 
 // export default configureStore;
 
+///////////////////////////////////////////////////////////////////////////////
 
-
+// import React, { Component } from "react";
 // import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 // import { connect } from "react-redux";
@@ -113,7 +114,113 @@ export function App (props: IAppProps) {
 // 	removeFromFavourites: (song) => {
 // 		dispatch(removeSongFromFavourites(song));
 // 	},
-// 	setPlaylist: (song, album) => {
-// 		dispatch(getPlaylistAction(song, album));
+// 	setPlaylist: (song, album, cover) => {
+// 		dispatch(getPlaylistAction(song, album, cover));
 // 	},
 // });
+
+// class Library extends Component {
+// 	state = {
+// 		loading: false,
+// 		id: this.props.match.params.id,
+// 		album: {},
+// 		artist: {},
+// 		tracks: [],
+// 	};
+
+// 	fetchAll = async () => {
+// 		try {
+// 			this.setState({ loading: true });
+// 			const resp = await fetch(
+// 				`https://striveschool-api.herokuapp.com/api/deezer/album/${this.props.match.params.id}`
+// 			);
+// 			const album = await resp.json();
+// 			this.setState({ loading: false });
+// 			this.setState({ album });
+// 			this.setState({ artist: album.artist });
+// 			this.setState({ tracks: album.tracks.data });
+// 			console.log(`fetch returns`, this.state.album, "tracks", this.state.tracks);
+// 		} catch (error) {
+// 			console.log(error);
+// 		}
+// 	};
+// 	componentDidMount = () => {
+// 		this.fetchAll();
+// 	};
+
+// 	render() {
+// 		return (
+// 			<div style={{ marginBottom: "200px" }}>
+// 				<div className='container-fluid m-0 p-0'>
+// 					<div className='row mt-5'>
+// 						<div className='col-lg-6 col-md-6 album-n'>
+// 							<div className='container m-0 p-1'>
+// 								<img src={this.state.album.cover_medium} id='img-album-n' alt='' />
+// 								<h4>{this.state.album.title}</h4>
+// 								<Link to={`/artist/${this.state.artist.id}`}>
+// 									<p className='fixLink'>{this.state.artist.name}</p>
+// 								</Link>
+// 								<button className='btn btn-success btn-n'>PLAY</button>
+// 								<p>
+// 									{this.state.album.fans} Fans • {this.state.album.nb_tracks} SONGS
+// 								</p>
+// 								<div></div>
+// 							</div>
+// 						</div>
+// 						<div className='col-lg-5 col-md-5 tracklist-n'>
+// 							<div className='track-container'>
+// 								<ul className=''>
+// 									{this.state.tracks.map((track) => {
+// 										return (
+// 											<p className='pointer'>
+// 												<span className='tracklist'>
+// 													<b>
+// 														<PlayCircle
+// 															onClick={() =>
+// 																this.props.setPlaylist(
+// 																	track,
+// 																	this.state.tracks,
+// 																	this.state.album.cover
+// 																)
+// 															}
+// 															className='iconHover mr-2'
+// 															size={16}
+// 														/>
+// 														{track.title_short}
+// 													</b>
+// 													<p style={{ color: "grey", fontSize: "12px" }}>
+// 														{Math.floor(track.duration / 60)}m{track.duration % 60}s{" "}
+// 														{this.props.favourites.songs
+// 															.map((s) => s.id)
+// 															.some((id) => id === track.id) && (
+// 															<HeartFill
+// 																className='iconHover ml-1 pointer'
+// 																size={16}
+// 																onClick={() => this.props.removeFromFavourites(track)}
+// 															/>
+// 														)}
+// 														{!this.props.favourites.songs
+// 															.map((s) => s.id)
+// 															.some((id) => id === track.id) && (
+// 															<Heart
+// 																className='iconHover ml-1 pointer'
+// 																size={16}
+// 																onClick={() => this.props.addToFavourites(track)}
+// 															/>
+// 														)}
+// 													</p>
+// 												</span>
+// 											</p>
+// 										);
+// 									})}
+// 								</ul>
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Library);
